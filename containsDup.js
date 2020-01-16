1135b428b39bf053b049e5f2f48ec3dc5520d01f
github username <matthewdizon94@protonmail.com>
2020-01-16 15:45:48 -0500

    ez

--- containsDup.js
+++ containsDup.js
@@ -6,5 +6,17 @@ in the array, and it should return false if every element is distinct.
 */
 
 const checker = (arr) => {
-    
+    let intObj = {}
+    for(let i = 0; i < arr.length; i++){
+        let item = arr[i]
+        if(intObj[item] === undefined){
+            intObj[item] = 1
+        }
+        else{
+            intObj[item] += 1
+            return true
+        }
+    }
+    return false
 }
+console.log(checker([1,2,3]))
