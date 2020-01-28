1b221c7bdbf63d9162bb4f88758a4651832870a5
github username <matthewdizon94@protonmail.com>
2020-01-27 20:54:02 -0500

    done

--- missingNum.js
+++ missingNum.js
@@ -1,5 +1,16 @@
 // Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
 
 const missing = (arr) => {
-
+    int sum = 0;
+    int idx = -1;
+    for (int i = 0; i < arr.length; i++)
+    {
+        if (arr[i] == 0)
+        {
+             idx = i; 
+        }
+        else 
+        {
+             sum += arr[i];
+     
 }
