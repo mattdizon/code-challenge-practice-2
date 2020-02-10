aad4147a37bd33711135a1a3513e9d7d943eb7ea
github username <matthewdizon94@protonmail.com>
2020-02-10 10:40:19 -0500

    working with stacks

--- validParen.js
+++ validParen.js
@@ -2,3 +2,27 @@
 
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
+
+/*
+given an array of different parenthesis
+the type ie (,[ ect
+the amount of the types such that opening = closing, 
+the order such that inner paren close before outer closes
+*/
+const isValid = (str) => {
+    let parenStack = []
+    for (let i=0; i < str.length; i++){
+        if(str[i] === '('  || str[i] === '{' || str[i] === '['){
+            parenStack.push(str[i])
+        }
+        else{
+            if(parenStack.length === 0) return false
+            let pair = parenStack.pop() + str[i]
+            if(pair !== '()' && pair !== '[]' && pair !== '{}' ){
+                return false
+            }
+        }
+    }
+    return parenStack.length === 0
+}
+console.log(isValid('[{]'))
